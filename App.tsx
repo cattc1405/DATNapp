52456926d22db6fe565d70567b20e0fcd90bdbfb
cattc1405 <cattc1405@gmail.com>
2024-09-14 23:07:16 +0700

    4 man hinh welcome va 2 steps register

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Profile from './src/Profile';
-
+import WelcomeScreen from './src/authen/Welcome/WelcomeScreen';
+import Welcome1 from './src/authen/Welcome/Welcome1';
+import Welcome2 from './src/authen/Welcome/Welcome2';
+import WelcomeNavigator from './src/authen/Welcome/WelcomeNavigator';
+import Step2Gender from './src/authen/Register/Step2Gender';
+import Step1Gender from './src/authen/Register/Step1Gender';
 
 function App(): React.JSX.Element {
-  return (
-  <Profile/>
-  );
+  return <Step1Gender />;
 }
 export default App;
-
--- src/test.js
+++
@@ -1,4 +0,0 @@
-let obj = { a: 1, b: 2 };
-let clone = Object.assign({}, obj);
-clone.a = 3;
-console.log(obj.a);
