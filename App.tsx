0cbf8035092200eac9a51b7fbd115d2d5c53bbbf
TruongAn-24 <ttan74437@gmail.com>
2024-10-25 12:28:26 +0700

    fix

--- App.tsx
+++ App.tsx
@@ -29,7 +29,7 @@ function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <StatusBar />
-      <Stack.Navigator initialRouteName="Payment5">
+      <Stack.Navigator initialRouteName="Code">
         <Stack.Screen name="ForgetPass" component={ForgetPass} options={{ headerShown: false }} />
         <Stack.Screen name="Code" component={Code} options={{ headerShown: false }} />
         <Stack.Screen name="NewPass" component={NewPass} options={{ headerShown: false }} />
